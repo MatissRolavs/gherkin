f7407781e80d73367a394e253c029e42787ee3ae
matissbby <matissrolavs2005@gmail.com>
2025-02-20 11:10:42 +0200

    upd

--- src/components/Question4.js
+++ src/components/Question4.js
@@ -7,11 +7,12 @@ const Question4 = () => {
         Kā Rubika kubs ļauj skatīties uz funkcionālajām iezīmēm?
       </p>
       <p className="question-text">
-        Rubika kubs ilustrē funkcionālās iezīmes, jo tā risināšana ir
-        deterministiska (tādi paši soļi dod tādu pašu rezultātu), izmanto
-        nemainīgumu (katrs pagrieziens rada jaunu stāvokli, bet sākotnējais
-        netiek mainīts) un funkciju kompozīciju (atsevišķas kustības apvienojas
-        sarežģītākos algoritmos).
+        Labs veids, kā vizualizēt scenārijus, ir izmantot Rubika kuba metaforu.
+        Iedomājies, ka tava funkcionalitāte ir Rubika kubs. Tu turi to rokās un
+        grozi, skatoties uz to no dažādiem leņķiem. Tu pamani, kā katrai pusei
+        ir dažādas krāsas un dažādi izkārtojumi. Tas ir tas pats kubs, bet katru
+        reizi, kad to pagriez, tu atklāj jaunus attēlu rakstus un šūnu
+        izvietojumus.
       </p>
     </div>
   );
